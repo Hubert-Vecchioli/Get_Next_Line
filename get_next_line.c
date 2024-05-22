46911c49b91033a3444bbb82c1cf298ca0f0bbc2
Hubert Vecchioli <hvecchio@made-f0Br4s9.clusters.42paris.fr>
2024-05-22 12:36:35 +0200

    Pre-work

--- get_next_line.c
+++ get_next_line.c
@@ -14,20 +14,82 @@
 
 char	*get_next_line(int fd)
 {
-   	char				*buffer;
-	ssize_t				bytesread;
+   	static char		*buffer[BUFFER_SIZE + 1];
+	char			*line;
+	size_t			bytesread;
 
-    // Check de protection
-    if (BUFFER_SIZE <= 0 || fd < 0)
+	if (BUFFER_SIZE <= 0 || fd < 0)
 		return (NULL);
-	buffer = malloc(sizeof(char) * BUFFER_SIZE);
-	if (!buffer)
-        return (NULL)
-    // lire le doc
-    bytesread = 1;
-    while () // continuer tant qu'on a pas de \n ou bytesread <0
-        bytesread = read(fd, buffer, BUFFER_SIZE);
-        // Ajouter dans un 2eme buffer ligne
-    // free buffer
-    //return the full line content
+	line = calloc(1, sizeof(char));
+	line = ft_strjoin(line, buffer);
+	if (line == NULL)
+		return (NULL);
+	bytesread = 1;
+	ft_bzero
+	while (bytes_read > 0 && ft_strchr_id(line, '\n') == -1)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
+			return (free(line), NULL);
+		buffer[bytes_read] = 0;
+		line = ft_strjoin(line, buffer)
+		if (line == NULL)
+			return (NULL);
+	ft_clean(line, buffer, '\n');
+	if (line[0] == 0)
+		return (NULL);
+	else
+		return (line);
+	}
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+		char    *result;
+
+		if (!s1 || !s2)
+			return (NULL);
+		result = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+		if (!result)
+				return (NULL);
+		ft_strlcpy(result, s1, ft_strlen(s1) + 1);
+		ft_strlcat(result, s2, ft_strlen(s2) + ft_strlen(s1) + 1);
+		return (result);
+}
+
+int	ft_strchr_id(char *str, char to_find)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (-1);
+	i = 0;
+	while (str[i] != (char)to_find)
+	{
+		if (str[i] == 0)
+			return (-1);
+		i++;
+	}
+	return (i);
+}
+void	ft_clean(char *line, char *buffer, char to_find)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (line[i] && line[i] != to_find)
+		i++;
+	if (line[i] == to_find)
+		i++;
+	while (line[i] && buffer[j])
+	{
+		buffer[j] = line[i];
+		line[i] = 0;
+		i++;
+		j++;
+	}
+	buffer[j] = 0;
 }
--- get_next_line.h
+++ get_next_line.h
@@ -17,6 +17,16 @@
 # define BUFFER_SIZE 100
 # endif
 
+# include <stdlib.h>
+
 char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+int     ft_strchr_id(char *str, char to_find);
+void	ft_clean(char *line, char *buffer, char to_find)
+void	*ft_calloc(size_t n, size_t size);
+void	ft_bzero(void *ptr, size_t n);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+size_t	ft_strlen(const char *str);
 
 #endif
